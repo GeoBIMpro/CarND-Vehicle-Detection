b8f59a1ea92a136e82a0b2fbf8acc0168c3de6ff
max <max@nutonomy.com>
2017-10-16 09:26:59 +0800

    heatmap

--- work.py
+++ work.py
@@ -14,90 +14,7 @@ from lesson_functions import *
 # from sklearn.model_selection import train_test_split
 from sklearn.cross_validation import train_test_split
 import pickle
-
-
-
-# Define a function to extract features from a single image window
-# This function is very similar to extract_features()
-# just for a single image rather than list of images
-def single_img_features(img, color_space='RGB', spatial_size=(32, 32),
-                        hist_bins=32, orient=9,
-                        pix_per_cell=8, cell_per_block=2, hog_channel=0,
-                        spatial_feat=True, hist_feat=True, hog_feat=True):
-    # 1) Define an empty list to receive features
-    img_features = []
-    # 2) Apply color conversion if other than 'RGB'
-    if color_space != 'RGB':
-        if color_space == 'HSV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-        elif color_space == 'LUV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
-        elif color_space == 'HLS':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-        elif color_space == 'YUV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-        elif color_space == 'YCrCb':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
-    else:
-        feature_image = np.copy(img)
-    # 3) Compute spatial features if flag is set
-    if spatial_feat == True:
-        spatial_features = bin_spatial(feature_image, size=spatial_size)
-        # 4) Append features to list
-        img_features.append(spatial_features)
-    # 5) Compute histogram features if flag is set
-    if hist_feat == True:
-        hist_features = color_hist(feature_image, nbins=hist_bins)
-        # 6) Append features to list
-        img_features.append(hist_features)
-    # 7) Compute HOG features if flag is set
-    if hog_feat == True:
-        if hog_channel == 'ALL':
-            hog_features = []
-            for channel in range(feature_image.shape[2]):
-                hog_features.extend(get_hog_features(feature_image[:, :, channel],
-                                                     orient, pix_per_cell, cell_per_block,
-                                                     vis=False, feature_vec=True))
-        else:
-            hog_features = get_hog_features(feature_image[:, :, hog_channel], orient,
-                                            pix_per_cell, cell_per_block, vis=False, feature_vec=True)
-        # 8) Append features to list
-        img_features.append(hog_features)
-
-    # 9) Return concatenated array of features
-    return np.concatenate(img_features)
-
-
-# Define a function you will pass an image
-# and the list of windows to be searched (output of slide_windows())
-def search_windows(img, windows, clf, scaler, color_space='RGB',
-                   spatial_size=(32, 32), hist_bins=32,
-                   hist_range=(0, 256), orient=9,
-                   pix_per_cell=8, cell_per_block=2,
-                   hog_channel=0, spatial_feat=True,
-                   hist_feat=True, hog_feat=True):
-    # 1) Create an empty list to receive positive detection windows
-    on_windows = []
-    # 2) Iterate over all windows in the list
-    for window in windows:
-        # 3) Extract the test window from original image
-        test_img = cv2.resize(img[window[0][1]:window[1][1], window[0][0]:window[1][0]], (64, 64))
-        # 4) Extract features for that window using single_img_features()
-        features = single_img_features(test_img, color_space=color_space,
-                                       spatial_size=spatial_size, hist_bins=hist_bins,
-                                       orient=orient, pix_per_cell=pix_per_cell,
-                                       cell_per_block=cell_per_block,
-                                       hog_channel=hog_channel, spatial_feat=spatial_feat,
-                                       hist_feat=hist_feat, hog_feat=hog_feat)
-        # 5) Scale extracted features to be fed to classifier
-        test_features = scaler.transform(np.array(features).reshape(1, -1))
-        # 6) Predict using your classifier
-        prediction = clf.predict(test_features)
-        # 7) If positive (prediction == 1) then save the window
-        if prediction == 1:
-            on_windows.append(window)
-    # 8) Return windows for positive detections
-    return on_windows
+from scipy.ndimage.measurements import label
 
 
 cars = []
@@ -301,6 +218,15 @@ elif method == 2:
     plt.show()
 else:
 
+    images = ['./test_images/test1.jpg',
+              './test_images/test2.jpg',
+              './test_images/test3.jpg']
+    # Check the prediction time for a single sample
+    t = time.time()
+
+    image = mpimg.imread('./test_images/test1.jpg')
+    draw_image = np.copy(image)
+
     ystart = 400
     ystop = 700
     scale = 2.0
@@ -308,6 +234,7 @@ else:
     colors = [(0, 0, 255), (0, 255, 0), (255, 0, 255)]
 
     hot_windows_list = []
+    hot_windows_list2 = []
     for scale in scales:
         tt = time.time()
         hot_windows = find_cars(image, ystart, ystop, scale, color_space, svc,
@@ -318,6 +245,7 @@ else:
         tt2 = time.time()
         print(round(tt2 - tt, 2), 'Seconds Scale {}'.format(scale))
         hot_windows_list.append(hot_windows)
+        hot_windows_list2.extend(hot_windows)
 
     t2 = time.time()
     print('Total',  round(t2 - t, 2), 'Seconds to Search ...')
@@ -332,4 +260,29 @@ else:
         plt.title('scale \n{}'.format(scales[idx-1]))
 
     plt.show()
+
+    heat = np.zeros_like(image[:, :, 0]).astype(np.float)
+    # Add heat to each box in box list
+    heat = add_heat(heat, hot_windows_list2)
+
+    # Apply threshold to help remove false positives
+    heat = apply_threshold(heat, 3)
+
+    # Visualize the heatmap when displaying
+    heatmap = np.clip(heat, 0, 255)
+
+    # Find final boxes from heatmap using label function
+    labels = label(heatmap)
+    draw_img = draw_labeled_bboxes(np.copy(image), labels)
+
+    fig = plt.figure()
+    plt.subplot(121)
+    plt.imshow(draw_img)
+    plt.title('Car Positions')
+    plt.subplot(122)
+    plt.imshow(heatmap, cmap='hot')
+    plt.title('Heat Map')
+    fig.tight_layout()
+    plt.show()
+
 
